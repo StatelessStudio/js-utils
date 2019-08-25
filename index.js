1b37b92b1a8f70ca1871ecf8a97ba1b91d2d533e
Drew Immerman <drewimmerman@gmail.com>
2019-08-25 04:41:38 -0400

    Added index files

---
+++ index.js
@@ -0,0 +1,1 @@
+exports.strIncludesElement = require('./src/string/str-includes-element');
---
+++ node/index.js
@@ -0,0 +1,3 @@
+exports.ignoreDirectoires = require('../src/node/ignore-directories');
+exports.replaceTextInDirectory = require('../src/node/replace-text-in-directory');
+exports.traverseDirectory = require('../src/node/traverse-directory');
