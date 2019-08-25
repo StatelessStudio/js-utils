603d3f59b4a797a7bd122ae89903915ed993fead
Drew Immerman <drewimmerman@gmail.com>
2019-08-25 04:41:29 -0400

    Created replaceTextInDirectory()

---
+++ src/node/replace-text-in-directory.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const traverseDirectory = require('./traverse-directory');
+
+/**
+ * Replace all occurrences of a string in the directory, recursively
+ * @param {string} dir Directory to replace in, recursively
+ * @param {RegExp} regex Regex string to replace
+ * @param {string} newstr New replacement text
+ */
+function replaceTextInDirectory(dir, regex, newstr) {
+	return new Promise((accept, reject) => {
+		traverseDirectory(dir, (file) => {
+			// TODO: Use r/w stream instead?
+			// Load file
+			fs.readFile(file, (error, data) => {
+				if (error) {
+					return reject(error);
+				}
+				else {
+					// Replace occurrences
+					const output = data.toString().replace(regex, newstr);
+
+					// Save file
+					fs.writeFile(file, output, (err) => {
+						return reject(error);
+					});
+				}
+			});
+		})
+			.then(accept)
+			.catch(reject);
+	});
+}
+module.exports = replaceTextInDirectory;
